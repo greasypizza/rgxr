5fdee6128e4b4be904576107a23d4084a374c4be
greasypizza <greasypizza@gmail.com>
2016-08-21 13:34:58 -0400

    forgot to delete these

--- include/rgxr-random.h
+++
@@ -1,2 +0,0 @@
-
-int getrandom_printable(int fd, char *buf, size_t buflen);
--- include/strtonum.h
+++
@@ -1,4 +0,0 @@
-
-long long
-strtonum(const char *numstr, long long minval, long long maxval,
-         const char **errstrp);
