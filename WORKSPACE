b716b4710aa32e29a9893d3eb769d14d99c06539
oppo <oppo@oppo.oppo>
2019-12-04 09:33:40 -0500

    use absl flags instead of gflags. bump re2 commit?

--- WORKSPACE
+++ WORKSPACE
@@ -1,24 +1,19 @@
 workspace(name = "greasypizza_rgxr")
 
+load("@bazel_tools//tools/build_defs/repo:git.bzl", "git_repository")
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
 git_repository(
     name = "com_googlesource_code_re2",
-    commit = "3d3d6faede5166fad62befda919edc3759294acc",
+    commit = "79ef3b2d31f06493f687ef9e947d9632bad54b9b",
     remote = "https://github.com/google/re2.git",
 )
 
-bind(
-    name = "re2",
-    actual = "@com_googlesource_code_re2//:re2",
+http_archive(
+    name = "com_google_absl",
+    sha256 = "c6a20dfedf17f3f64b090388bdb12ebdfe63e9e1e2249b813bce2e2228a77e27",
+    strip_prefix = "abseil-cpp-a4b757b5d42694306a9de853cee0a5fba9c7bbe9",
+    urls = [
+        "https://github.com/abseil/abseil-cpp/archive/a4b757b5d42694306a9de853cee0a5fba9c7bbe9.zip",
+    ],
 )
-
-git_repository(
-    name = "gflags_github",
-    commit = "a69b2544d613b4bee404988710503720c487119a",
-    remote = "https://github.com/gflags/gflags.git",
-)
-
-bind(
-    name = "gflags",
-    actual = "@gflags_github//:gflags",
-)
-
--- rgxr/main.cc
+++ rgxr/main.cc
@@ -1,33 +1,39 @@
 #include <iostream>
 #include <random>
+#include <string>
 
-#include "gflags/gflags.h"
+#include "absl/flags/flag.h"
+#include "absl/flags/parse.h"
+#include "absl/flags/usage.h"
 #include "re2/re2.h"
 
-DEFINE_int32(len, 15, "Length of generated password");
-DEFINE_string(expr, ".*", "RE2 expression");
+ABSL_FLAG(int, len, 15, "Length of generated password");
+ABSL_FLAG(std::string, expr, ".*", "RE2 expression");
 
 constexpr char kMinPrintable = '!';
 constexpr char kMaxPrintable = '~';
 constexpr char kUsage[] = "Generate password matching RE2 expressions.";
 
-int main(int argc, char** argv) {
-  gflags::SetUsageMessage(kUsage);
-  gflags::ParseCommandLineFlags(&argc, &argv, true);
+int main(int argc, char **argv) {
+  absl::SetProgramUsageMessage(kUsage);
+  absl::ParseCommandLine(argc, argv);
 
-  RE2 expr(FLAGS_expr, RE2::Quiet);
+  const std::string expr_str = absl::GetFlag(FLAGS_expr);
+  const int len = absl::GetFlag(FLAGS_len);
+
+  RE2 expr(expr_str, RE2::Quiet);
   if (!expr.ok()) {
     std::cout << expr.error() << std::endl;
-    gflags::ShowUsageWithFlagsRestrict(argv[0], "rgxr");
+    // gflags::ShowUsageWithFlagsRestrict(argv[0], "rgxr");
     return -1;
   }
 
-  if (FLAGS_len < 1) {
+  if (len < 1) {
     std::cout << "len must be > 0" << std::endl;
-    gflags::ShowUsageWithFlagsRestrict(argv[0], "rgxr");
+    // gflags::ShowUsageWithFlagsRestrict(argv[0], "rgxr");
     return -1;
   }
-  std::string result(FLAGS_len, kMinPrintable);
+  std::string result(len, kMinPrintable);
 
   std::random_device rd;
   std::uniform_int_distribution<char> dist(kMinPrintable, kMaxPrintable);
